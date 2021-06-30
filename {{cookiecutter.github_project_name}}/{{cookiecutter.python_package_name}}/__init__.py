05cf686f2692cea118cc23bfcf9aed6230a0c868
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-06-30 14:31:05 -0400

    Always import `__version__` first in `__init__`
    Based on https://github.com/jupyter-widgets/ipywidgets/blob/bf56eb7a09cc7f1572e50b5bd618d57cfa9a334f/ipywidgets/__init__.py#L20-L21

--- {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/__init__.py
+++ {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/__init__.py
@@ -4,5 +4,9 @@
 # Copyright (c) {{ cookiecutter.author_name }}.
 # Distributed under the terms of the Modified BSD License.
 
-from .example import example_function
+# Must import __version__ first to avoid errors importing this file during the build process. 
+# See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__
+
+from .example import example_function
+
